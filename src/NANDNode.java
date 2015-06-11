23e2c8e040380a52b389af3d06eee75cb8788546
Sagar Gangabisoensingh <sagartje1993@gmail.com>
2015-06-11 04:12:41 +0200

    Alle gates(moeten nog getest worden)

---
+++ src/NANDNode.java
@@ -0,0 +1,67 @@
+/**
+ * Created by Sagar on 11-6-2015.
+ */
+public class NANDNode extends Node {
+    public NANDNode() {
+        maxInputs = 2;
+        value = 2;
+        inputs = new Input[2];
+    }
+
+    @Override
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public void update(Input input) {
+        if(inputs[0] == input || inputs[1] == input){
+            calculateValue();
+        } else {
+            System.out.println("Error, ANDGate krijgt een update van een node waaraan hij niet gekoppeld is");
+        }
+    }
+
+    @Override
+    public void pair(Input newInput) {
+        if (inputs[0] == null){
+            inputs[0] = newInput;
+            inputs[0].registerObserver(this);
+        } else if (inputs[1] == null){
+            inputs[1]= newInput;
+            inputs[1].registerObserver(this);
+        } else {
+            System.out.println("Error, ANDGate is vol en probeert een input erbij te zetten");
+        }
+    }
+
+    @Override
+    public void unpair(Input oldInput) {
+        if (inputs[0] == oldInput){
+            inputs[0].unregisterObserver(this);
+            inputs[0] = null;
+        } else if (inputs[1] == oldInput){
+            inputs[1].unregisterObserver(this);
+            inputs[1] = null;
+        } else {
+            System.out.println("Error, ANDGate probeerd een input te unpairen die niet gepaired is");
+        }
+    }
+
+    @Override
+    public void calculateValue() {
+        int newValue;
+        if (inputs[0] == null || inputs[1] == null){
+            newValue = 2;
+        } else if (inputs[0].getValue() != 1 && inputs [1].getValue() != 1) {
+            newValue = 1;
+        } else {
+            newValue = 0;
+        }
+
+        if (value != newValue){
+            value = newValue;
+            notifyObservers();
+        }
+    }
+}
